e90a3ea8150d4937cfdbe4b069a531594b22193a
aline <alineesilva998@gmail.com>
2025-07-05 19:18:19 -0300

    Adicionar comentário explicativo no script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Quando o botão for clicado, mostra uma mensagem de boas-vindas
 document.getElementById('botaoMensagem').addEventListener('click', function() {
     document.getElementById('mensagem').innerText = "Olá, é um prazer receber você aqui!😊";
 });
